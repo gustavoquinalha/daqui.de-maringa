00664af7b03f2583b80b073fc87edb939f367534
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2020-04-01 20:27:20 -0300

    update pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,8 @@
       <n-link class="btn m-2 bg-purple-600 hover:bg-purple-500 text-white" to="/">index</n-link>
       <n-link class="btn m-2 bg-purple-600 hover:bg-purple-500 text-white" to="/app">app</n-link>
       <n-link class="btn m-2 bg-purple-600 hover:bg-purple-500 text-white" to="/cadastro">cadastro</n-link>
+      <n-link class="btn m-2 bg-purple-600 hover:bg-purple-500 text-white" to="/admin">admin</n-link>
+      <n-link class="btn m-2 bg-purple-600 hover:bg-purple-500 text-white" to="/auth">auth</n-link>
     </div>
 
     <nuxt class="container mx-auto pt-4" />
@@ -13,17 +15,28 @@
 <script>
 export default {
   head: {
-    title: "Nuxt.js with TailwindCSS",
+    title: 'Serviços daqui de Maringá',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: "description",
-        name: "description",
-        content: "Nuxt.js with TailwindCSS example"
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: ''
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ]
   }
-};
+}
 </script>
--- pages/admin.vue
+++ pages/admin.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-5xl">Cadastro</h1>
+    <h1 class="text-5xl">Admin</h1>
   </div>
 </template>
 
